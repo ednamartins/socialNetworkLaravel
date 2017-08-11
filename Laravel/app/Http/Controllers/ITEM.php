eb25fdf0ae646f501e891fe9cc8a211f769d29d9
ednamartins <Eliedygaspar@gmail.com>
2017-08-11 16:00:53 +0100

    othes

---
+++ Laravel/app/Http/Controllers/ITEM.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Laravel_Teste\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ITEM extends Controller
+{
+    public function index(){
+      echo 'index';
+   }
+   public function create(){
+      echo 'create';
+   }
+   public function store(Request $request){
+      echo 'store';
+   }
+   public function show($id){
+      echo 'show';
+   }
+   public function edit($id){
+      echo 'edit';
+   }
+   public function update(Request $request, $id){
+      echo 'update';
+   }
+   public function destroy($id){
+      echo 'destroy';
+   }
+}
